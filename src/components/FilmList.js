09098540d058a8ca5d7ec7d452d81256075127cf
Myriam Boran <brnmyriam45@googlemail.com>
2019-05-28 08:51:36 +0100

    homework

---
+++ src/components/FilmList.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Film from "./Film";
+
+class FilmList extends Component {
+  render() {
+    const filmNodes = this.props.data.map(film => {
+      return (
+        <Film url={film.url} key={film.id}>
+          {film.name}
+        </Film>
+      );
+    });
+    return (
+      <div className="film-list">
+        <h1>Upcoming movies</h1>
+        <ul>{filmNodes}</ul>
+      </div>
+    );
+  }
+}
+
+export default FilmList;
---
+++ src/components/Films.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Films extends Component {
+  render() {
+    return <a href="https://www.imdb.com/trailers/?ref_=nv_mv_tr">More></a>;
+  }
+}
+
+export default Films;
